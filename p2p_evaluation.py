f3b5eceaeb2f2ffb2a726f782f332fcf9cc36bd9
JuanseHevia <jh216@rice.edu>
2024-11-28 20:30:54 -0600

    Add sampling option to evaluation dataset and update argument parser

--- p2p_evaluation.py
+++ p2p_evaluation.py
@@ -17,6 +17,9 @@ def evaluate(args):
     dataset = utils.MBDataset(data_dir=args.data_dir)
     evaluation_dataset = dataset.build_evaluation_dataset()
 
+    # sample dataset
+    evaluation_dataset = evaluation_dataset.sample(n=args.sample_size)
+
     # load pipeline
     pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(
         "timbrooks/instruct-pix2pix", torch_dtype=torch.float16
@@ -62,6 +65,7 @@ if __name__ == "__main__":
     parser.add_argument("--dump_every", type=int, default=500)
     parser.add_argument("--inference_steps", type=int, default=50)
     parser.add_argument("--savedir", type=str, default="results")
+    parser.add_argument("--sample_size", type=int, default=1000)
     args = parser.parse_args()
 
     evaluate(args)
